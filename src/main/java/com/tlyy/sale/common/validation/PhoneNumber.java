80c777346c0ae5ab02c76d65b8ed47a3c8842ef4
leidongxing <leidongxing@gmail.com>
2020-06-21 23:45:35 +0800

    参数校验

---
+++ src/main/java/com/tlyy/sale/common/validation/PhoneNumber.java
@@ -0,0 +1,18 @@
+package com.tlyy.sale.common.validation;
+
+import javax.validation.Constraint;
+import java.lang.annotation.*;
+
+/**
+ * @author LeiDongxing
+ * create on 2020/6/21 23:41
+ */
+@Documented
+@Constraint(validatedBy = PhoneNumberValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PhoneNumber {
+    String message() default "Invalid phone number";
+    Class[] groups() default {};
+    Class[] payload() default {};
+}
